92b96e8dda8e31c9070bec0838f2dcd98904ed23
Henrique <henrique.ph935@gmail.com>
2024-10-08 11:27:15 -0300

    FEAT: Finaliza docker

---
+++ .docker/php-fpm/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Instalando dependências do Composer..."
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Definir permissões corretas
+echo "Definindo permissões para as pastas de cache e armazenamento do Laravel..."
+
+# Permitir ao servidor web (www-data ou nginx) acessar essas pastas
+HTTP_USER=www-data
+
+# Definir permissões para as pastas de cache e storage
+chown -R $USER:$HTTP_USER bootstrap/cache
+chown -R $USER:$HTTP_USER storage
+
+chmod -R 775 bootstrap/cache
+chmod -R 775 storage
+
+php-fpm
